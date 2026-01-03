38844214e34aec03f61a55d7fd1eb64ba64b372b
Arch me <kishudwivedi30@gmail.com>
2026-01-03 23:08:28 +0530

    textnode

--- src/textnode.py
+++ src/textnode.py
@@ -4,10 +4,9 @@ class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
-    CODE = "CODE"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
-
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -17,12 +16,10 @@ class TextNode:
 
     def __eq__(self, other):
         return (
-                self.text == other.text
-                and self.text_type == other.text_type
-                and self.url == other.url
+            self.text == other.text 
+            and self.text_type == other.text_type 
+            and self.url == other.url
         )
 
     def __repr__(self):
-        return f"TextNone({self.text}, self.text_type, self.url)"
-
-
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
