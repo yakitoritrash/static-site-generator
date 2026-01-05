92f5422612d700308f7c2a61ac7d8d2b4d48b00d
Arch me <kishudwivedi30@gmail.com>
2026-01-05 21:45:26 +0530

    Leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,15 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("No value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -50,6 +51,19 @@ class TestHTMLNode(unittest.TestCase):
                 "HTMLNode(p, What a strange world, None, {'class': 'primary'})"
                 )
 
-
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "click me", {"href": "https://google.com"})
+        self.assertEqual(node.to_html(), '<a href = "https://google.com">click me</a>')
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just raw text")
+        self.assertEqual(node.to_html(), "Just raw text")
+    def test_leaf_to_html_no_value_raises_error(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 if __name__ == "__main__":
     unittest.main()
